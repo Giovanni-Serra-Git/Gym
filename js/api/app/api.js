d9b8924dc4faa437a233354f63ad35282e2b7e6c
Giovanni-Serra-Git <155887277+Giovanni-Serra-Git@users.noreply.github.com>
2024-10-18 14:43:16 +0200

    Update api.js

--- js/api/app/api.js
+++ js/api/app/api.js
@@ -1,5 +1,40 @@
-const apiKey = "3dbabcb1c2msh7cad4344bbc778fp173816jsn519c1879608";
+const apiKey = "3dbabcb1c2msh7cad4344bbc778fp173816jsn519c18796089";
 const apiKeyExercises = "3dbabcb1c2msh7cad4344bbc778fp173816jsn519c18796089";
+
+const url = 'https://exercisedb.p.rapidapi.com/status';
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '3dbabcb1c2msh7cad4344bbc778fp173816jsn519c18796089',
+		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
+const url_2 = 'https://youtube-search-results.p.rapidapi.com/youtube-search/?q=justin%2Bbieber';
+const options_2 = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '3dbabcb1c2msh7cad4344bbc778fp173816jsn519c18796089',
+		'x-rapidapi-host': 'youtube-search-results.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url_2, options_2);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
 
 
 export const rapidApiUrl = `exercisedb.p.rapidapi.com`
